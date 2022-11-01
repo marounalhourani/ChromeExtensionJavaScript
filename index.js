920e1a91574ecbd355c66149b6bc0ba9d4f7668b
maroun <alhouranimaroun@gmail.com>
2022-11-01 13:15:04 +0200

    finalized version

--- index.js
+++ index.js
@@ -1,44 +1,63 @@
 let myLeads = []
+let odlLeads = []
+const tabBtn = document.getElementById("tab-btn")
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+tabBtn.addEventListener("click", function()
+{
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
+}
+)
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 console.log(leadsFromLocalStorage)
 
 if(leadsFromLocalStorage){
   myLeads = leadsFromLocalStorage
-  renderLeads()
+  render(myLeads)
 }
 
+//render the leads in the ul using ulEl.textContent
+function render(leads) {
+  let listItems = ""
+    for(let i = 0; i < leads.length; i++)
+       {
+        //this is called template string/ template literals
+          listItems +=
+          `
+          <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+          </li>
+          `}
+          ulEl.innerHTML = listItems
+        }
+
+
+deleteBtn.addEventListener("dblclick", function()
+  {
+    console.log("hello dbl")
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)  }
+)
 
 inputBtn.addEventListener("click", function(){
   myLeads.push(inputEl.value)
   console.log(myLeads)
-  renderLeads()
+  render(myLeads)
   inputEl.value = ""
-  localStorage.setItem("myLeads",JSON.stringify(myLeads))
+  localStorage.setItem("myLeads",JSON.stringify(myLeads))})
 
 
-
-     
-})
-
-//render the leads in the ul using ulEl.textContent
-function renderLeads() {
-    let listItems = ""
-      for(let i = 0; i < myLeads.length; i++)
-         {
-          //this is called template string/ template literals
-            listItems +=
-            `
-            <li>
-              <a target='_blank' href='${myLeads[i]}'>
-                  ${myLeads[i]}
-              </a>
-            </li>
-            `
-            ulEl.innerHTML = listItems
 //or we can do it this way
 //  ListItems += "<li>" + "<a target='_blank' href='" + myLeads[i] + "'>" + myLeads
 // [i] + "</a>" +"</li>"}
@@ -48,7 +67,7 @@ function renderLeads() {
 // li.textContent = myLeads[i]
 // ulEl.append(li)
 
-          }}
+ 
     
       
 
